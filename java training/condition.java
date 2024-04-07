53ddb2866e1daccca29ef7eb861e26d08493daa9
THIRUMALAIPRIYA <113224619+THIRUMALAIPRIYA@users.noreply.github.com>
2024-04-07 22:29:03 +0530

    Add files via upload

---
+++ java training/condition.java
@@ -0,0 +1,14 @@
+class condition{
+   public static void main(String args[]){
+        System.out.println("ambalaya");
+        if("ambalaya"){
+           System.out.println("vaa");
+}
+        else{
+           System.out.println("varadha");
+}
+
+
+}
+
+}
